78de886e3fe8c1adff189f02d8c9006eca4ec8f2
S7IMOUCH <keefqth@gmail.com>
2025-02-16 22:56:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function play(){
+        alert("for a better expreince listen")
+        let audio=new audio("5 Minute Timer Lofi (320).mp3");
+            audio.play()}
+play.addEventListener(playmusic)
